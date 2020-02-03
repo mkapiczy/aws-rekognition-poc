477d839e51787858d70c66957030deacc4a3d8bd
mkapiczy <mix.kap@gmail.com>
2020-02-03 11:34:13 +0000

    Init project

---
+++ src/main/java/pl/grapeup/awsrekognitionpoc/AwsRekognitionPocApplication.java
@@ -0,0 +1,13 @@
+package pl.grapeup.awsrekognitionpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsRekognitionPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsRekognitionPocApplication.class, args);
+	}
+
+}
